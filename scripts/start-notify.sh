56358271532bbb82f1b82768799f6314c67be085
albertodicaprio <albert.cirstoiu@gmail.com>
2026-09-19 19:19:45 +0200

    cleanup System chat notification message

--- scripts/start-notify.sh
+++ scripts/start-notify.sh
@@ -11,7 +11,7 @@ trap 'exit 0' TERM INT
 tail --pid="$$" -F "$DATA_DIR/logs/latest.log" | while read -r line; do
   if [[ "$line" == *"<"*">"* || "$line" == *"joined the game"* || "$line" == *"left the game"* || "$line" == *"This server is running Paper version"* || "$line" == *"Stopping server" ]]; then
     echo "$line"
-    line_clean="$(echo "$line" | sed -E 's/^\[[0-9:]+\] \[[^]]+\]: (\[Not Secure\] )?//')"
+    line_clean="$(echo "$line" | sed -E 's/^\[[0-9:]+\] \[[^]]+\]: (System chat: )?(\[Not Secure\] )?//')"
     echo "SEND: $line_clean"
     curl -fsS -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
       -d "chat_id=${TELEGRAM_CHAT_ID}" \
